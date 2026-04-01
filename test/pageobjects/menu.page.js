917f45da49afc08245e398db37c53b0630b30342
dillonfuller8095-cloud <dillon.fuller8095@stu.mtec.edu>
2026-04-01 13:47:09 -0600

    Refactor getter methods for consistency

--- test/pageobjects/menu.page.js
+++ test/pageobjects/menu.page.js
@@ -2,35 +2,34 @@ import { $ } from '@wdio/globals'
 
 class MenuPage {
 
-    get allItems () {
+    get allItems() {
         return $('#inventory_sidebar_link')
     }
 
-    get about () {
+    get about() {
         return $('#about_sidebar_link')
     }
 
-    get logout () {
+    get logout() {
         return $('#logout_sidebar_link')
     }
 
-    get resetApp () {
+    get resetApp() {
         return $('#reset_sidebar_link')
     }
 
-    async clickAllItems () {
-        await this.allItems.click()
-    }
-
-    async clickAbout () {
+    async clickAbout() {
+        await this.about.waitForClickable({ timeout: 10000 })
         await this.about.click()
     }
 
-    async clickLogout () {
+    async clickLogout() {
+        await this.logout.waitForClickable({ timeout: 10000 })
         await this.logout.click()
     }
 
-    async clickReset () {
+    async clickReset() {
+        await this.resetApp.waitForClickable({ timeout: 10000 })
         await this.resetApp.click()
     }
 }
