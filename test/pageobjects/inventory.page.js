61665d7bd9283dfdfd5e08f2815935e7aa48e0a7
dillonfuller8095-cloud <dillon.fuller8095@stu.mtec.edu>
2026-04-01 13:44:36 -0600

    Refactor InventoryPage with new item management methods
    Refactor InventoryPage to include new item methods and update existing ones.

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -3,28 +3,69 @@ import Page from './page.js'
 
 class InventoryPage extends Page {
 
-    get burgerMenu () {
+    // elements
+    get inventoryContainer() {
+        return $('#inventory_container')
+    }
+
+    get burgerMenu() {
         return $('#react-burger-menu-btn')
     }
 
-    get cartIcon () {
+    get cartIcon() {
         return $('.shopping_cart_link')
     }
 
-    get addToCartButton () {
-        return $('button*=Add to cart')
+    get addBackpack() {
+        return $('#add-to-cart-sauce-labs-backpack')
     }
 
-    async openMenu () {
+    get removeBackpack() {
+        return $('#remove-sauce-labs-backpack')
+    }
+
+    get addBikeLight() {
+        return $('#add-to-cart-sauce-labs-bike-light')
+    }
+
+    // actions
+    async waitForPageLoaded() {
+        await this.inventoryContainer.waitForDisplayed({ timeout: 10000 })
+    }
+
+    async openMenu() {
+        await this.burgerMenu.waitForClickable({ timeout: 10000 })
         await this.burgerMenu.click()
     }
 
-    async addItemToCart () {
-        await this.addToCartButton.click()
+    async addItemToCart() {
+        await this.addBackpack.waitForClickable({ timeout: 10000 })
+        await this.addBackpack.click()
     }
 
-    async openCart () {
+    async removeItemFromCart() {
+        await this.removeBackpack.waitForClickable({ timeout: 10000 })
+        await this.removeBackpack.click()
+    }
+
+    async addMultipleItems() {
+        await this.addBackpack.click()
+        await this.addBikeLight.click()
+    }
+
+    async rapidClickAdd(times = 5) {
+        for (let i = 0; i < times; i++) {
+            await this.addBackpack.click()
+        }
+    }
+
+    async openCart() {
+        await this.cartIcon.waitForClickable({ timeout: 10000 })
         await this.cartIcon.click()
+    }
+
+    open() {
+        return super.open('inventory.html')
     }
 }
 
