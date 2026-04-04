cbabb651b0feb06cb94bd5267abce95046a7a7b0
dillonfuller8095-cloud <dillon.fuller8095@stu.mtec.edu>
2026-04-03 23:34:28 -0600

    Refactor menu page methods for element visibility
    Updated clickReset, clickLogout, and clickAbout methods to wait for elements to be displayed before clicking.

--- test/pageobjects/menu.page.js
+++ test/pageobjects/menu.page.js
@@ -5,12 +5,22 @@ class MenuPage {
     get allItems ()  { return $('#inventory_sidebar_link') }
     get logout ()    { return $('#logout_sidebar_link') }
     get resetApp ()  { return $('#reset_sidebar_link') }
-    get reset ()     { return $('#reset_sidebar_link') }
     get about ()     { return $('#about_sidebar_link') }
 
-    async clickReset ()  { await this.reset.click() }
-    async clickLogout () { await this.logout.click() }
-    async clickAbout ()  { await this.about.click() }
+    async clickReset () {
+        await this.resetApp.waitForDisplayed({ timeout: 10000 })
+        await this.resetApp.click()
+    }
+
+    async clickLogout () {
+        await this.logout.waitForDisplayed({ timeout: 10000 })
+        await this.logout.click()
+    }
+
+    async clickAbout () {
+        await this.about.waitForDisplayed({ timeout: 10000 })
+        await this.about.click()
+    }
 
 }
 
